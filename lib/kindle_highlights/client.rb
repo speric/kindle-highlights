5258994965ef36daabfd3d71ddf109ad5c83fccc
natto <natto@natto-tpy.none>
2016-02-11 10:35:33 -0500

    Change initialize to allow mechanize configuration options

--- lib/kindle_highlights/client.rb
+++ lib/kindle_highlights/client.rb
@@ -5,12 +5,12 @@ module KindleHighlights
 
     attr_reader :books
         
-    def initialize(email_address, password)
+    def initialize(email_address, password, **mechanize_kwargs)
       @email_address = email_address
       @password      = password
       @books         = Hash.new
       
-      setup_mechanize_agent
+      setup_mechanize_agent(mechanize_kwargs)
       load_books_from_kindle_account
     end
   
@@ -52,10 +52,15 @@ module KindleHighlights
       end
     end
     
-    def setup_mechanize_agent
+    def setup_mechanize_agent(mechanize_options)
       @mechanize_agent                        = Mechanize.new
       @mechanize_agent.user_agent_alias       = 'Windows Mozilla'
       @mechanize_agent.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      # Override default settings using mechanize_options
+      mechanize_options.each do |mech_attr, value|
+        @mechanize_agent.send("#{mech_attr}=", value)
+      end
     end
   end
 end
